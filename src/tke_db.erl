47bd8a8cfcbb67e6196efc56d595ebb876289a51
fred <fred@fred-desktop.(none)>
2012-10-25 22:07:57 +0200

    feature "update" in progress

--- src/tke_db.erl
+++ src/tke_db.erl
@@ -10,7 +10,13 @@
 -export([start/0, stop/0]).
 -export([init/1, handle_call/3, handle_cast/2]).
 -export([code_change/3, handle_info/2, terminate/2]).
--export([get/3]).
+
+-export([get/3, update/2]).
+
+-record(project, {name, issues, messages}).
+-record(issue, {id, title, status, owner, summary, ctime, tags}).
+-record(message, {id, author, ctime, contents}).
+%% contents is either: {file, Filename} | {text, Text} | {update, Text}
 
 %% API -------------------
 
@@ -29,8 +35,8 @@ get(Project, message, N) ->
 
 % if new issue, then create and return id.
 % if existing issue, then update
-update(Project, issue, Issue) ->
-    gen_server:call(Project, {update, Issue}).
+update(Project, Item) ->
+    gen_server:call(Project, {update, Item}).
 
 search(Project, Search) ->
     gen_server:call(Project, {search, Search}).
@@ -42,22 +48,27 @@ init(Project) ->
     log:debug("init(~p)", [Project]),
     % TODO load from disk and populate an ets table
     {Issues, Messages} = load(Project),
-    {ok, {Issues, Messages}}.
+    {ok, #project{name=Project, issues=Issues, messages=Messages}}.
 
-handle_call({get, issue, N}, _From, {Issues, _Messages}) ->
-    case ets:lookup(Issues, N) of
+handle_call({get, issue, N}, _From, Ctx) ->
+    case ets:lookup(Ctx#project.issues, N) of
         [I] -> ok;
         [] -> I = none
     end,
-    {reply, I, {Issues, _Messages}};
-handle_call({get, message, N}, _From, Chs) ->
-    {reply, {message, N}, Chs};
-handle_call({update, _Issue}, _From, Chs) ->
-    {reply, {ok, 333}, Chs};
-handle_call({search, _Search}, _From, Chs) ->
-    {reply, [], Chs}.
+    {reply, I, Ctx};
+handle_call({get, message, N}, _From, Ctx) ->
+    {reply, {message, N}, Ctx};
+handle_call({update, I = #issue{id=new}}, _From, Ctx) ->
+    New_id = get_new_id(Ctx#project.issues),
+    I2 = I#issue{id=New_id},
+    ets:insert(Ctx#project.issues, I2),
+    log:debug("going to sync..."),
+    sync(Ctx#project.name, I2),
+    {reply, {ok, I2}, Ctx};
+handle_call({search, _Search}, _From, Ctx) ->
+    {reply, [], Ctx}.
 
-handle_cast(stop, State) -> {stop, normal, State};
+handle_cast(stop, Ctx) -> {stop, normal, Ctx};
 handle_cast(_X, Y) -> {noreply, Y}.
 
 %% File access functions
@@ -90,6 +101,31 @@ load_issues_from_files([File | Others], Issue_table) ->
 
 load_messages(_Project, _Message_table) -> todo.
 
+%% write to disk what has been modified
+sync(Project, I = #issue{}) -> 
+    log:debug("syncing..."),
+    Id = integer_to_list(I#issue.id),
+    Filename = Project ++ "/" ++ Id ++ "/issue",
+        %% TODO create dir if needed
+    Str = io_lib:format("~p.", [I]),
+    Bytes = list_to_binary(Str),
+    X=file:write_file(Filename, Bytes),
+    log:debug("syncing...~p, Filename=~p", [X, Filename]),
+    X;
+sync(Project, I = #message{}) -> ok.
+
 code_change(_, _, _) -> ok.
 handle_info(_, _) -> ok.
 terminate(shutdown, _State) -> ok.
+
+%% Get new id functions
+get_new_id(Issues) -> get_max_id(Issues, ets:first(Issues), 0) + 1.
+
+get_max_id(Issues, '$end_of_table', N) -> N;
+get_max_id(Issues, Key, N) ->
+    case Key > N of
+        true -> Max = Key;
+        _Else -> Max = N
+    end,
+    get_max_id(Issues, ets:next(Issues, Key), Max).
+
